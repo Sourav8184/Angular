0bfb4d31b6d1c402af5b1c721d1236a557ab24b9
Sourav <ksourav662@gmail.com>
2025-01-21 11:43:21 +0530

    Set up Angular project with default structure Add user and admin components for practice and understanding

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,5 +4,22 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  /*
+    List of providers that should be available to the root component
+    and all its children.
+  */
+  providers: [
+    /*
+      Provides NgZone-based change detection for the application
+      bootstrapped using bootstrapApplication.NgZone is already provided
+      in applications by default.
+    */
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    /*
+      Sets up providers necessary to enable Router functionality for the
+      application. Allows to configure a set of routes as well as extra
+      features that should be enabled.
+    */
+    provideRouter(routes),
+  ],
 };
